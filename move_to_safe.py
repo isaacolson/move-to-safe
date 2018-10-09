b19015fcfa4fd12fbf8f3dae12f45f50827ca4a6
isaacolson <olsonites@gmail.com>
2018-10-09 16:31:28 -0500

    Update move_to_safe.py

--- move_to_safe.py
+++ move_to_safe.py
@@ -11,11 +11,11 @@ import requests
 
 pos_x = device.get_current_position('x')
 pos_y = device.get_current_position('y')
-pos_z = device.get_current_position('z')
+pos_z = z_height
 
 device.move_absolute(pos_x, pos_y, pos_z)
 
 if __name__ == '__main__':
     farmware_name = 'move_to_safe'
     # Load inputs from Farmware page widget specified in manifest file
-    z_pos = get_env('safe_z')
+    z_height = get_env('safe_z')
